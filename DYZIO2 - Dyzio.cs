2aa6f0b8986a21df0dfa5bbeb731a4dbbab72022
petardaOFF <officialpetarda@gmail.com>
2021-07-04 13:58:19 +0200

    Add files via upload
    A tutaj akurat przekroczony czas mimo wielu prób zmieszczenia się w nim. Kod kilka razy przebudowywałem, zmieniałem, skracałem, ale nic to nie dało. Po przekroczeniu 15000 testów "2 1000000" przekraczam czas.

---
+++ DYZIO2 - Dyzio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DYZIO2___Dyzio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+               Console.WriteLine(IsPrime());
+            }
+            
+            
+      
+        }
+
+        static int IsPrime()
+        {
+            string[] range = Console.ReadLine().Split(' ');
+            int min = int.Parse(range[0]);
+            int max = int.Parse(range[1]);
+            int counter = 0;
+            bool[]  tab= new bool[max + 1];
+
+            for (int i = 2; i * i <= max; i++)
+            {
+                if (tab[i] == true)
+                {
+                    continue;
+                }
+                for (int j = 2 * i; j <= max; j += i)
+                {
+                   
+                }
+            }
+
+           
+            
+
+
+
+
+
+            return counter;
+
+        }
+    }
+}
